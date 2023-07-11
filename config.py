555d27f669583080448105bdfc82d88c0ec1709d
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2023-07-12 01:47:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5437374877"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://luffyx0o:@sangtam933911@cluster0.ejoy3le.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Luciferbin:9339119160@cluster0.kmbiojj.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "luffyx0o")
 
 #force sub channel id, if you want enable force sub
