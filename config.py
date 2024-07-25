ee9e5e628ffef52d725a515e17adeb498c60a994
ATOMIC-00 <132604794+ATOMIC-00@users.noreply.github.com>
2024-07-25 23:57:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Luciferbin:9339119160@clu
 DB_NAME = os.environ.get("DATABASE_NAME", "AiFileShare")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002015985192"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002154284624"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
